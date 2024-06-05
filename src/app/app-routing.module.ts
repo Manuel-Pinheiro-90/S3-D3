dbd18287959964bdbefa388373e2a0993fe237d8
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-06-05 16:45:54 +0200

    funzione minima

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuestGuard } from './auth/guest.guard';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
- { path: 'utente', loadChildren: () => import('./pages/utente/utente.module').then(m => m.UtenteModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+ { path: 'utente', loadChildren: () => import('./pages/utente/utente.module').then(m => m.UtenteModule),
+ canActivate: [AuthGuard],
+ canActivateChild: [AuthGuard]
+  },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  canActivate: [GuestGuard],
+  canActivateChild: [GuestGuard],
+   }
 ];
 
 @NgModule({
